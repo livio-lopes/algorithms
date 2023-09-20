7d04a3bca7314ee1d92768b32318184999587373
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-20 19:57:31 -0300

    feat(palindrome): implemented is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
+    if word == "":
+        return False
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     """Faça o código aqui."""
-    raise NotImplementedError
+    # raise NotImplementedError
+
+
+print(is_palindrome_recursive("ABA", -1, 0))
