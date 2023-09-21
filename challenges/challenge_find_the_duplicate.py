170e2e084988c2df1a9a66f17e2566b9306ddd4d
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-21 17:27:00 -0300

    feat(find_duplicate): implement find number is duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,19 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if isinstance(nums, list) and len(nums) >= 2:
+        set_nums = set()
+        nums.sort()
+        for num in nums:
+            if isinstance(num, str) or num < 0:
+                return False
+            if num in set_nums:
+                return num
+            set_nums.add(num)
+        return False
+    else:
+        return False
+
+
+# test = [3, 1, 2, 4, 6, 5, 7, 7, 7, 8]
+# #  test = [1,2,3,4,5]
+
+# print(find_duplicate(test))
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    word_to_list = [letter for letter in word]
+    reverse = word_to_list.reverse()
+    reverse = "".join(reverse)
+    return reverse == word
