cdf2488b7a9ff361c9e1c69b4f5818ff771a6840
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-21 17:45:36 -0300

    feat(is_palindrome_iterative): implement function iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,7 +1,12 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-    word_to_list = [letter for letter in word]
-    reverse = word_to_list.reverse()
-    reverse = "".join(reverse)
-    return reverse == word
+    mid = len(word) // 2
+    if len(word) % 2 == 0:
+        half_right_word = word[mid:]
+    else:
+        half_right_word = word[mid + 1:]
+    half_right_word = [letter for letter in half_right_word]
+    half_right_word.reverse()
+    half_right_word = "".join(half_right_word)
+    return word[:mid] == half_right_word
