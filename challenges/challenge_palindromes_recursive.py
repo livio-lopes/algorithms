22cff32c9b41a483633e5f674321533772131923
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-21 07:24:01 -0300

    docs(palindromes): define default params and explanation of applied logic

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,26 @@
-def is_palindrome_recursive(word, low_index, high_index):
+# define default params low_index = right index and high_index = left index
+def is_palindrome_recursive(word, low_index=-1, high_index=0):
+    # business rule
     if word == "":
         return False
+
+    # ensure that the word in the input is always upper case
+    word = word.upper()
+    """ from now on it is the logic of the prompt used in chatGPT 3.5
+        'complete in python def is_palindrome_recursive(word, low_index,
+        high_index):' """
+
+    """ base case """
+    # compare low_index and high_index
     if low_index >= high_index:
         return True
+    # compare letter right and letter right
     if word[low_index] != word[high_index]:
         return False
+    # state change and called recursion
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    """Faça o código aqui."""
-    # raise NotImplementedError
 
 
-# print(is_palindrome_recursive("ABA", -1, 0))
+# test = "abC"
+# print(is_palindrome_recursive(test))
+# print(is_palindrome_recursive(test := "aba"))
