1fa47359211f9ee177014ca1b17477e53e1279a6
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-21 17:53:17 -0300

    chore(is_palindromes_interative): resolves error lint flake8

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -5,7 +5,7 @@ def is_palindrome_iterative(word):
     if len(word) % 2 == 0:
         half_right_word = word[mid:]
     else:
-        half_right_word = word[mid + 1:]
+        half_right_word = word[mid + 1 :]
     half_right_word = [letter for letter in half_right_word]
     half_right_word.reverse()
     half_right_word = "".join(half_right_word)
